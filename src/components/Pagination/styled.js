055cf1d34fa97737f8bece4629e9632e1f627a2e
Vituriano <viturianooliveira@gmail.com>
2020-04-26 05:25:01 -0300

    bug(master): change config files

--- src/components/Pagination/styled.js
+++ src/components/Pagination/styled.js
@@ -6,7 +6,7 @@ export const PaginationWrapper = styled.section`
   border-top: 1px solid var(--borders);
   color: var(--texts);
   display: flex;
-  padding: 1.5rem 3rem;
+  padding: 1.5rem 6.75rem 1.5rem 23rem;
   justify-content: space-between;
 
   a{
